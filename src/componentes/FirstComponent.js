80f89ffda5af8449cd6ffcaa61890d02a6b416da
Jefferson Lima <jbldesenvolvimento@gmail.com>
2024-09-03 17:32:07 -0300

    declaração de className e não só class

--- src/componentes/FirstComponent.js
+++ src/componentes/FirstComponent.js
@@ -2,8 +2,11 @@ const FirstComponent = () => {
   // Todo Componente tem que ter o return
   return (
     // Dentro do return e tudo tags de HTML que serão exibidas no navegador - se da o nome de JSX
+    // Não declarar como class e sim como className pois da erro de performance e erro no navegador no console declarando só class
     <div>
       <h1> Meu Primeiro Componente</h1>
+
+      <p className="teste"> Meu Texto de teste</p>
     </div>
   );
 };
