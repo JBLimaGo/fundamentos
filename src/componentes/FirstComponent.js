a4f6717f0436a4e5649495e4cc2712b11162a43f
Jefferson Lima <jbldesenvolvimento@gmail.com>
2024-09-03 17:24:25 -0300

    JSX dentro do return dos componentes

--- src/componentes/FirstComponent.js
+++ src/componentes/FirstComponent.js
@@ -1,6 +1,7 @@
 const FirstComponent = () => {
   // Todo Componente tem que ter o return
   return (
+    // Dentro do return e tudo tags de HTML que serão exibidas no navegador - se da o nome de JSX
     <div>
       <h1> Meu Primeiro Componente</h1>
     </div>
