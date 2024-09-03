94dfd3a45d123c191bf8694f504b32433d8f42cb
Jefferson Lima <jbldesenvolvimento@gmail.com>
2024-09-03 17:13:34 -0300

    Criação de Componentes e Utilização

---
+++ src/componentes/FirstComponent.js
@@ -0,0 +1,10 @@
+const FirstComponent = () => {
+  // Todo Componente tem que ter o return
+  return (
+    <div>
+      <h1> Meu Primeiro Componente</h1>
+    </div>
+  );
+};
+
+export default FirstComponent;
